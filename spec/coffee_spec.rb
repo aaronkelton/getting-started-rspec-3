d16e61be1d13f7dd2723476f2095e309d7e95aa2
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-08-21 21:03:09 -0500

    add some focus to temporarily run only tagged examples use the --only-failures tag to avoid running entire test suite

--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -8,18 +8,25 @@ class Coffee
   end
 
   def price
-    1.00
+    price = 1.00
+    price += 0.25 if self.ingredients.include? :milk
+    price
   end
+end
+
+RSpec.configure do |config|
+  config.filter_run_when_matching(focus: true)
+  config.example_status_persistence_file_path = 'spec/examples.txt'
 end
 
 RSpec.describe 'A cup of coffee' do
   let(:coffee) { Coffee.new }
 
-  it 'costs $1' do
+  it 'costs $1', focus: true do
     expect(coffee.price).to eq(1.00)
   end
 
-  context 'with milk' do
+  fcontext 'with milk' do
     before { coffee.add :milk }
 
     it 'costs $1.25' do
